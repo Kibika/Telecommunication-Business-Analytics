ba8d4b72ef0decc0ae493b840f024cee69abc50f
Stella Kibika <stellakibika@gmail.com>
2021-07-17 16:21:58 +0300

    User overview dashboard page

--- app/dashboard/layout.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
-
-import app.static.functions as funcs
-
-df = pd.read_csv("/data/Week1_challenge_data.csv")
--- app/dashboard/layouts/data_cleaning.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as pd
-import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
-
-import app.static.functions as funcs
-
-data=pd.read_csv("/data/Week1_challenge_data.csv")
